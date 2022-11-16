c9daafa565d33f9b8b6f8366a25ea9903dc35134
Rohit Kumar Bind <90304020+bindrohit07@users.noreply.github.com>
2022-11-16 14:05:11 +0530

    Add files via upload

---
+++ src/Components/navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <>
+    <nav> 
+        <div>
+            <button> Breakfast</button>
+            <button> Lunch</button>
+            <button> eveningSnacks</button>
+            <button> Dinner</button>
+            <button> Dinner</button>
+        </div>
+    </nav>
+    </>
+  )
+}
+
+export default navbar
